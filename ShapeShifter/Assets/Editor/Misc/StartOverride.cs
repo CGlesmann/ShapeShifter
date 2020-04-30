458be626604db0941d103f9e6ae21be95e4c8892
LightAxe2402 <chrisglesmann@gmail.com>
2020-04-29 19:53:34 -0700

    Even More Editor Improvements
    Uploaded Editor Enhancer Package
    Configed Side bar tool with overloaded PlayMode initializer
    Dark Skin!

---
+++ ShapeShifter/Assets/Editor/Misc/StartOverride.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.IO;
+
+public static class StartOverride
+{
+    private const string textFilePath = "Editor Tools/ReloadData.json";
+    private static string fullTextFilePath => "Assets/Editor Default Resources/" + textFilePath;
+
+    private const string MAIN_MENU_PATH = "Assets/GameScenes/Menu Scenes/MainMenu.unity";
+
+    [MenuItem("Start From Main Menu", menuItem = "Global Tools/Start Main Menu")]
+    public static void StartFromMainMenu()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            StreamWriter writer = new StreamWriter(fullTextFilePath);
+            writer.WriteLine(EditorSceneManager.GetActiveScene().path);
+            writer.Close();
+
+            AssetDatabase.ImportAsset(fullTextFilePath);
+
+            EditorSceneManager.OpenScene(MAIN_MENU_PATH);
+            EditorApplication.isPlaying = true;
+        }
+    }
+
+    [InitializeOnLoadMethod]
+    public static void SubscribeReloadFunction() { Debug.Log("Subscribing ReloadInitialScene"); EditorApplication.playModeStateChanged += ReloadInitialScene;}
+    public static void ReloadInitialScene(PlayModeStateChange mode)
+    {
+        Debug.Log($"Entering {mode}");
+        if (mode == PlayModeStateChange.EnteredEditMode)
+        {
+            StreamReader reader = new StreamReader(fullTextFilePath);
+            string sceneToLoad = reader.ReadLine();
+            reader.Close();
+
+            Debug.Log($"Attempting to load scene at {sceneToLoad}");
+            EditorSceneManager.OpenScene(sceneToLoad);
+        }
+    }
+}
